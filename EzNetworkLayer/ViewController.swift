cdab7a177883d41ebc4423aab8c26169fcebc304
PinYuan <champion790713@gmail.com>
2022-02-21 08:59:43 +0800

    create project and implement Moya

---
+++ EzNetworkLayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EzNetworkLayer
+//
+//  Created by Patrick Chen on 2022/2/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-EzNetworkLayer/Pods-EzNetworkLayer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EzNetworkLayer : NSObject
+@end
+@implementation PodsDummy_Pods_EzNetworkLayer
+@end
